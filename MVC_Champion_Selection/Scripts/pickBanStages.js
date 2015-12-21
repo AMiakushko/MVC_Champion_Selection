1366848c462dd758e253c59bdb3bc4b8d7009b44
Alex Miakushko <aleksey.miakushko@gmail.com>
2015-12-21 20:20:15 +0200

    missing pickBanStages.js fix

---
+++ MVC_Champion_Selection/Scripts/pickBanStages.js
@@ -0,0 +1,88 @@
+﻿
+// pick ban order is set here
+// id: slot in team
+// type: ban or pick
+// side: blue or red
+
+var pickBanStages = [
+    {
+        id: "1",
+        type: "ban",
+        side: "blue"
+    },
+    {
+        id: "1",
+        type: "ban",
+        side: "red"
+    },
+    {
+        id: "2",
+        type: "ban",
+        side: "blue"
+    },
+    {
+        id: "2",
+        type: "ban",
+        side: "red"
+    },
+    {
+        id: "3",
+        type: "ban",
+        side: "blue"
+    },
+    {
+        id: "3",
+        type: "ban",
+        side: "red"
+    },
+    {
+        id: "1",
+        type: "pick",
+        side: "blue"
+    },
+    {
+        id: "1",
+        type: "pick",
+        side: "red"
+    },
+    {
+        id: "2",
+        type: "pick",
+        side: "red"
+    },
+    {
+        id: "2",
+        type: "pick",
+        side: "blue"
+    },
+    {
+        id: "3",
+        type: "pick",
+        side: "blue"
+    },
+    {
+        id: "3",
+        type: "pick",
+        side: "red"
+    },
+    {
+        id: "4",
+        type: "pick",
+        side: "red"
+    },
+    {
+        id: "4",
+        type: "pick",
+        side: "blue"
+    },
+    {
+        id: "5",
+        type: "pick",
+        side: "blue"
+    },
+    {
+        id: "5",
+        type: "pick",
+        side: "red"
+    }
+];
